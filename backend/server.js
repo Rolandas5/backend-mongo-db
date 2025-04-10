da576d1a63fff2abf81f43f1549c5c3d6350f137
Rolandas5 <r.bendorius@gmail.com>
2025-04-10 13:25:48 +0300

    backend-mongo-db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: '../.env' });
+console.log('DEBUG MONGO_URI:', process.env.MONGO_URI);
 
 const PORT = 3000;
 
@@ -7,6 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// MongoDB prisijungimas
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// MongoDB prisijungimas
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ Prisijungta prie MongoDB!');
+
+    // Startuojam serverį tik kai prisijungiam prie DB
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error('MongoDB klaida:', err));
